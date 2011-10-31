42e482246e4d7500ed07d1956dda4620ffd273ae
Simão Neto <simaomenezesneto@gmail.com>
2011-10-31 12:54:47 -0300

    Arquivo de criação do banco

---
+++ ci_doctrine_db.sql
@@ -0,0 +1,27 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+DROP SCHEMA IF EXISTS 'ci_doctrine' ;
+CREATE SCHEMA IF NOT EXISTS 'ci_doctrine' DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE 'ci_doctrine' ;
+
+-- -----------------------------------------------------
+-- Table 'ci_doctrine'.'User'
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 'ci_doctrine'.'User' ;
+
+CREATE  TABLE IF NOT EXISTS 'ci_doctrine'.'User' (
+  'id' INT NOT NULL AUTO_INCREMENT ,
+  'username' VARCHAR(45) NULL ,
+  'password' VARCHAR(45) NULL ,
+  'first_name' VARCHAR(45) NULL ,
+  'last_name' VARCHAR(45) NULL ,
+  PRIMARY KEY ('id') )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
